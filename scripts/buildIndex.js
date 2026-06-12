6ad024e1fec11923076084d067d945516a3a94e5
jannat21 <davood.jannat@gmail.com>
2026-06-12 08:20:17 -0700

    modify embeding

--- scripts/buildIndex.js
+++ scripts/buildIndex.js
@@ -1,11 +1,11 @@
 const { loadDocuments } = require("../src/documentLoader");
-const { LocalEmbeddings } = require("../src/localEmbeddings");
+const { createEmbedder  } = require("../src/localEmbeddings");
 const { buildStore } = require("../src/vectorStore");
 
 async function run() {
 
-  const embedder = new LocalEmbeddings();
-  await embedder.init();
+  const embedder = await createEmbedder();
+  //await embedder.init();
 
   const docs = await loadDocuments();
 
